023b643c4224de5c8a6f9b6f8e13c8cd76621841
cryptowyrm <cryptowyrm@protonmail.ch>
2017-11-09 19:57:10 +0100

    first commit

---
+++ src/steemobservatory/client.cljs
@@ -0,0 +1,80 @@
+(ns steemobservatory.client
+  (:require [reagent.core :as r]
+            [cljs.pprint :as pprint]))
+
+(enable-console-print!)
+
+(defonce articles (r/atom []))
+(defonce avatar (r/atom ""))
+
+(defn parseAvatarUrl [account]
+  (let [parsed (js/JSON.parse (get account "json_metadata"))
+        meta (js->clj parsed)]
+    (get-in meta ["profile" "profile_image"])))
+
+(defn getDiscussions []
+  (.then
+    (js/steem.database.getDiscussions
+      "blog"
+      (clj->js {:limit 10
+                :tag "crypticwyrm"}))
+    (fn [result]
+      (swap! articles
+        (fn []
+          (map js->clj result))))
+    (fn [e] (js/console.log e))))
+
+(defn getAccounts []
+  (.then
+    (js/steem.database.getAccounts (clj->js ["crypticwyrm"]))
+    (fn [result]
+      (reset! avatar
+        (parseAvatarUrl
+          (js->clj (first result)))))
+    (fn [e] (js/console.log e))))
+
+(defn is-article-active [article]
+  (let [cashout (js/Date. (get article "cashout_time"))
+        now (js/Date.)]
+    (> (- cashout now) 0)))
+
+(defn article-item [article]
+  (let [cashout (js/Date. (get article "cashout_time"))
+        now (js/Date.)
+        active (> (- cashout now) 0)
+        worth (if active
+                (get article "pending_payout_value")
+                (get article "total_payout_value"))]
+    [:div {:class "article"
+           :on-click (fn []
+                       (js/console.log (clj->js article)))}
+     [:span {:class (if active
+                      "votes active"
+                      "votes")}
+      (get article "net_votes")]
+     [:div {:class "right"}
+      [:span {:class "title"}
+       (get article "title")]
+      [:span {:class "worth"}
+       worth]]]))
+
+(defn list-articles [articles]
+  [:div {:class "article-list"}
+   (for [[index article] (map-indexed vector articles)]
+     ^{:key index}
+     [article-item article])])
+
+(defn content []
+  [:div {:style {:background "#ddd"
+                 :padding "20px"}}
+   [:img {:src @avatar
+          :style {:width "100px"}}]
+   [list-articles @articles]
+   [:button {:on-click getDiscussions}
+    "getDiscussions"]
+   [:button {:on-click getAccounts}
+    "getAccounts"]])
+
+(r/render-component [content]
+  (.querySelector js/document "#app"))
+
